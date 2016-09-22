67bb0a3fed85a1df7d7f101aab999bb5dd0c5702
eder <eder@air.local>
2016-09-22 18:55:52 -0400

    update setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = [
 
 setup(
     name='merkletools',
-    version='1.0.0',
+    version='1.0.1',
     description='Merkle Tools',
     classifiers=[
         "Intended Audience :: Developers",
