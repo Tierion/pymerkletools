b065bfd398adf031603fb362ef4e7b848966cec9
eder.santana <edercsjr@gmail.com>
2018-02-13 20:16:43 -0800

    update setup.py with more precise info

--- setup.py
+++ setup.py
@@ -14,11 +14,15 @@ setup(
     description='Merkle Tools',
     classifiers=[
         "Intended Audience :: Developers",
+        "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules"
     ],
-    url='https://github.com/',
+    url='https://github.com/Tierion/pymerkletools',
     author='Eder Santana',
     keywords='merkle tree, blockchain, tierion',
     license="MIT",
